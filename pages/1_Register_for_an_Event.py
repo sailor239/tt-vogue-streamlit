c3270c83b66f82383ce2de314a597db0490c5b56
sailor239 <67307263+sailor239@users.noreply.github.com>
2023-05-26 19:52:21 +0800

    Create 1_Register_for_an_Event.py

---
+++ pages/1_Register_for_an_Event.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+import json
+from streamlit_modal import Modal
+import streamlit.components.v1 as components
+
+
+st.set_page_config(
+    page_title='TT Vogue',
+    page_icon='👋',
+)
